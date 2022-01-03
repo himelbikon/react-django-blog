e0ae5390014eef7ed640d0d0aa52453ed3714206
himelbikon <skhimelhaque@gmail.com>
2022-01-03 23:35:24 +0600

    paginating

--- frontend/src/components/MiddleBar.jsx
+++ frontend/src/components/MiddleBar.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 import axios from "axios";
 import { Pagination } from "react-bootstrap";
 import PostCard from "./PostCard";
 
+import "../scss/MiddleBar.scss";
+
 const MiddleBar = () => {
   const [blogs, setBlogs] = useState([]);
+  const [page, setPage] = useState("");
+
+  const [searchParams] = useSearchParams();
 
   const get_blogs = async () => {
     await axios
@@ -24,23 +31,72 @@ const MiddleBar = () => {
       });
   };
 
+  // const paginate = () => {
+  //   setPage(searchParams.get("page"));
+  //   if (page === null) {
+  //     setPage(1);
+  //   }
+  // };
+
   useEffect(() => {
     get_blogs();
-  }, []);
+
+    // setPage(searchParams.get("page"));
+    if (searchParams.get("page")) {
+      setPage(searchParams.get("page"));
+    } else {
+      setPage(1);
+    }
+
+    // paginate();
+  }, [searchParams, setPage]);
 
   return (
     <div>
-      {blogs.map((blog) => (
+      {/* {blogs.map((blog) => (
         <PostCard blog={blog} key={blog.id} />
-      ))}
+      ))} */}
 
-      <Pagination className="justify-content-center">
-        <Pagination.First />
-        <Pagination.Prev />
-        <Pagination.Item>{1}</Pagination.Item>
+      <Pagination className="justify-content-center my-pagi">
+        {parseInt(page) != 1 > 0 ? (
+          <LinkContainer to={`/?page=${1}`}>
+            <Pagination.First />
+          </LinkContainer>
+        ) : null}
 
-        <Pagination.Item active>{2}</Pagination.Item>
-        <Pagination.Item>{3}</Pagination.Item>
+        {parseInt(page) - 1 > 0 ? (
+          <LinkContainer to={`/?page=${parseInt(page) - 1}`}>
+            <Pagination.Prev />
+          </LinkContainer>
+        ) : null}
+
+        {parseInt(page) - 3 > 0 ? (
+          <LinkContainer to={`/?page=${parseInt(page) - 3}`}>
+            <Pagination.Item>{parseInt(page) - 3}</Pagination.Item>
+          </LinkContainer>
+        ) : null}
+        {parseInt(page) - 2 > 0 ? (
+          <LinkContainer to={`/?page=${parseInt(page) - 2}`}>
+            <Pagination.Item>{parseInt(page) - 2}</Pagination.Item>
+          </LinkContainer>
+        ) : null}
+        {parseInt(page) - 1 > 0 ? (
+          <LinkContainer to={`/?page=${parseInt(page) - 1}`}>
+            <Pagination.Item>{page - 1}</Pagination.Item>
+          </LinkContainer>
+        ) : null}
+
+        <Pagination.Item active>{page}</Pagination.Item>
+
+        <LinkContainer to={`/?page=${parseInt(page) + 1}`}>
+          <Pagination.Item>{parseInt(page) + 1}</Pagination.Item>
+        </LinkContainer>
+        <LinkContainer to={`/?page=${parseInt(page) + 2}`}>
+          <Pagination.Item>{parseInt(page) + 2}</Pagination.Item>
+        </LinkContainer>
+        <LinkContainer to={`/?page=${parseInt(page) + 3}`}>
+          <Pagination.Item>{parseInt(page) + 3}</Pagination.Item>
+        </LinkContainer>
 
         <Pagination.Next />
         <Pagination.Last />
